78dda8617f4decaaf857de0f0a48c49c965cc437
NicolJamie <jamie.nicol@cigroup.co.uk>
2018-03-05 21:54:49 +0000

    ServiceProvider

--- src/SpacesServiceProvider.php
+++ src/SpacesServiceProvider.php
@@ -14,18 +14,20 @@ class SpacesServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/spaces.php' => config_path('spaces.php'),
-        ], 'spaces');
+            __DIR__.'/../config/mongo.php' => config_path('mongo.php'),
+        ], 'mongo');
     }
 
     /**
      * Register the application services.
      *
      * @return void
+     * @throws \Exception
      */
     public function register()
     {
-        // Detect if AWS is installed
-        // Load the main class
+        if (!class_exists('Aws\S3\S3Client')) {
+            throw new  \Exception('AWS SDK is not found, please require');
+        }
     }
 }
