2c99e95d54f83fc1855cae00afa9b3c4c823c0fd
NicolJamie <jamie.nicol@cigroup.co.uk>
2018-03-05 21:48:15 +0000

    spaces config

---
+++ config/spaces.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'accessKey' => null,
+    'secretKey' => null,
+    'space' => null,
+    'region' => null,
+    'host' => 'digitaloceanspaces.com'
+];
---
+++ src/SpacesServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace NicolJamie\Spaces;
+
+use Illuminate\Support\ServiceProvider;
+
+class SpacesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Get settings from the configuration file
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Detect if AWS is installed
+        // Load the main class
+    }
+}
